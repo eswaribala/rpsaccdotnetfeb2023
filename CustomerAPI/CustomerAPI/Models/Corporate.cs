ffcd3a54474266192980c79e421361fa838ec4c9
eswaribala <parameswaribala@gmail.com>
2023-02-06 16:34:34 +0530

    updated

--- CustomerAPI/CustomerAPI/Models/Corporate.cs
+++ CustomerAPI/CustomerAPI/Models/Corporate.cs
@@ -1,6 +1,12 @@
-﻿namespace CustomerAPI.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CustomerAPI.Models
 {
+    public enum CompanyType { Public,NGO,MNC}
+    [Table("Corporate")]
     public class Corporate:Customer
     {
+        [Column("CompanyType")]
+        public CompanyType CompanyType { get; set; }
     }
 }
