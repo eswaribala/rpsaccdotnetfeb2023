14b6e4bc91636568c2d1045e0f737a16b608a0e5
eswaribala <parameswaribala@gmail.com>
2023-02-06 16:23:50 +0530

    updated

--- CustomerAPI/CustomerAPI/Models/Individual.cs
+++ CustomerAPI/CustomerAPI/Models/Individual.cs
@@ -1,6 +1,14 @@
-﻿namespace CustomerAPI.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CustomerAPI.Models
 {
+    public enum Gender { MALE,FEMALE,TRANSGENDER}
+    [Table("Individual")]
     public class Individual:Customer
     {
+        [Column("Gender")]
+        public Gender Gender { get; set; }
+        [Column("DOB")]
+        public DateTime DOB { get; set; }
     }
 }
