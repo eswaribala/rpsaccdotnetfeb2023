bc7d604c795cd8b401d6076e967b6388d6f7b84c
eswaribala <parameswaribala@gmail.com>
2023-02-07 11:06:31 +0530

    updated

---
+++ CustomerAPI/CustomerAPI/Controllers/CustomersController.cs
@@ -0,0 +1,57 @@
+﻿using CustomerAPI.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CustomerAPI.Controllers
+{
+    [ApiVersion("1.0")]
+    [ApiVersion("1.1")]
+    [ApiVersion("2.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]  
+    public class CustomersController : ControllerBase
+    {
+
+        private ICustomerRepo customerRepo;
+        private IConfiguration configuration;
+
+        public CustomersController(ICustomerRepo _customerRepo, IConfiguration _configuration)
+        {
+            this.customerRepo = _customerRepo;
+            this.configuration = _configuration;
+        }
+
+        // GET: api/<CustomersController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<CustomersController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CustomersController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CustomersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CustomersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- CustomerAPI/CustomerAPI/Program.cs
+++ CustomerAPI/CustomerAPI/Program.cs
@@ -1,4 +1,7 @@
 using CustomerAPI.Contexts;
+using CustomerAPI.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +16,16 @@ builder.Services.AddDbContext<BankingContext>(options => options
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddTransient<ICustomerRepo, CustomerRepo>();
+//builder.Services.AddApiVersioning();
+builder.Services.AddApiVersioning(x =>
+{
+    x.DefaultApiVersion = new ApiVersion(1, 0);
+    x.AssumeDefaultVersionWhenUnspecified = true;
+    x.ReportApiVersions = true;
+    x.ApiVersionReader = new HeaderApiVersionReader("x-api-version");
+});
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
@@ -25,6 +38,15 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors(builder =>
+{
+    builder
+    .WithOrigins("http://localhost:4200", "http://localhost:3000", "http://localhost:7072")
+    .WithMethods("PUT", "DELETE", "GET", "POST")
+    .AllowAnyHeader();
+
+
+});
 
 app.UseAuthorization();
 
