355c2efcab7ac308278f21a37c109449fbe859f6
eswaribala <parameswaribala@gmail.com>
2023-02-10 12:20:47 +0530

    updated

---
+++ CsharpFundamentals/CsharpFundamentals/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Top level Statements
+
+//Console.WriteLine("Hello, World!");
+using System;
+namespace CsharpFundamentals
+{
+
+    //This describes c# Entrypoint
+    /*
+     * I am a beginner
+     * Training Started on 06/02/2023
+     */
+   /// <summary>
+   /// This is Main
+   /// </summary>
+  ///<remarks>Kick Started</remarks>
+    enum AccountType { SAVINGS,CURRENT,DEMAT,LOAN}
+    public class Fundamental
+    {
+
+        public static void Main()
+        {
+            Console.WriteLine("Ready to Learn c#");
+            double ROI = 0.65;
+            //Enter Account Type
+            Console.WriteLine("Enter Account Type");
+            string? value= Console.ReadLine();
+            AccountType result,accountType;
+            if(Enum.TryParse<AccountType>(value,out result))
+            {
+                accountType = result;
+                Console.WriteLine("Account Type{0}", accountType);
+
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
