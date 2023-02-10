0259f0b1a21b5b03c40917dcdab74c186824095e
eswaribala <parameswaribala@gmail.com>
2023-02-10 14:43:34 +0530

    updated

---
+++ CsharpFundamentals/CsharpFundamentals/Models/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpFundamentals.Models
+{
+    public enum TransactionType { ATM,DEBITCARD,CREDITCARD,WALLET}
+    public  class Transaction
+    {
+        public long TransactionId { get; set; } 
+        public long TransactionAmount { get; set; }
+        public TransactionType TransactionType { get; set; }
+
+        public DateTime DOT { get; set; }   
+
+        //association
+
+        public Account Account { get; set; }
+
+    }
+}
