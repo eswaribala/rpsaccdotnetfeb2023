2a03ea6e09afac23ffc8a77bc6d3e2f8dfc27b61
eswaribala <parameswaribala@gmail.com>
2023-02-10 13:10:25 +0530

    updated

---
+++ CsharpFundamentals/CsharpFundamentals/Models/CurrentAccount.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpFundamentals.Models
+{
+    public class CurrentAccount:Account
+    {
+        public long OverDraftLimit { get; set; }
+    }
+}
