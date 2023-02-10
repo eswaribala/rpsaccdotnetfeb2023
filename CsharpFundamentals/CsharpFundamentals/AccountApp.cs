0d1030e577af99d65bacfaef0150cc70c06f2688
eswaribala <parameswaribala@gmail.com>
2023-02-10 17:03:07 +0530

    updated

--- CsharpFundamentals/CsharpFundamentals/AccountApp.cs
+++ CsharpFundamentals/CsharpFundamentals/AccountApp.cs
@@ -34,6 +34,31 @@ namespace CsharpFundamentals
                     Account.AccountId,Account.AccountType,Account.Balance,Account.DOC);
                 Console.WriteLine("Generated OTP={0}", Account.GenerateOTP());
             }
+
+            IDictionary<long, Transaction> Transactions = new Dictionary<long, Transaction>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                Transactions.Add(i, new Transaction()
+                {
+                    TransactionId = new Random().Next(1, 10000000),
+                    TransactionAmount = new Random().Next(500, 5000000),
+                    DOT = new DateTime(2023, 2, new Random().Next(1, 25)),
+                    Account = new CurrentAccount(100, AccountType.SAVINGS,
+                  new Random().Next(5000, 10000),
+                  new DateTime(2023, 2, new Random().Next(1, 25)))
+                });
+
+            }
+
+            //Retrieve Transactions
+            foreach(KeyValuePair<long,Transaction> transaction in Transactions){
+
+                Console.WriteLine("Key{0},Transaction Id{1}, Amount{2}",
+                   transaction.Key, transaction.Value.TransactionId, transaction.Value.TransactionAmount);
+            }
+
+
         }
     }
 }
